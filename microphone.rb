f9265faf5c5b99dd14c416c2e8a8c8349e5737dc
andrey9828 <aaa73731919@gmail.com>
2016-02-14 22:02:37 +0200

    added microphone

---
+++ microphone.rb
@@ -0,0 +1,11 @@
+class Microphone
+  attr_accessor :frequency_band, :sensitivity
+
+  def test_record
+    puts 'запись началась'
+    sleep 3
+    unless @frequency_band || @sensitivity == nil
+      puts 'запись успешно завершена'
+    end
+  end
+end
