c58792f983241e2f6db44e951ea54ee619965bbf
andrey9828 <aaa73731919@gmail.com>
2016-02-14 22:21:46 +0200

    update speaker

--- speaker.rb
+++ speaker.rb
@@ -1,3 +1,11 @@
 class Speaker < Headset
   attr_accessor :frequency_band, :max_input_power
+
+  def test_sound
+    puts 'воспроизвидение звука началось'
+    sleep 3
+    unless @frequency_band || @max_input_power == nil
+      puts 'воспроизвидение успешно завершено'
+    end
+  end
 end
