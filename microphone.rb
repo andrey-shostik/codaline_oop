b8d8f43c2f459a26a4a54cce5379d3de175c36c0
andrey9828 <aaa73731919@gmail.com>
2016-04-11 16:34:30 +0300

    init commit test branch

--- microphone.rb
+++ microphone.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Microphone
   attr_accessor :frequency_band, :sensitivity
 
@@ -5,6 +6,15 @@ class Microphone
     puts 'тест записи микрафона, запись началась'
     sleep 3
     unless @frequency_band && @sensitivity == nil
+=======
+class Microphone < Headset
+  attr_accessor :frequency_band, :sensitivity
+
+  def test_record
+    puts 'запись началась'
+    sleep 3
+    unless @frequency_band || @sensitivity == nil
+>>>>>>> microphone
       puts 'запись успешно завершена'
     end
   end
